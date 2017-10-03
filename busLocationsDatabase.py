dc2b12af0886fa5baa372728a9c0001051c6c212
JonathanERuhnke <jrlucky@gmail.com>
2017-10-03 14:08:25 -0700

    Update busLocationsDatabase.py

--- busLocationsDatabase.py
+++ busLocationsDatabase.py
@@ -1,4 +1,5 @@
 def getBusPositionData(routeId, key):
+	import urllib
 	from urllib.request import urlopen
 	import numpy as np
 	import xml.etree.ElementTree as ET
@@ -8,72 +9,80 @@ def getBusPositionData(routeId, key):
 	## http://api.pugetsound.onebusaway.org/api/where/trips-for-route/40_100236.xml?key=TEST&includeStatus=true
 	## http://api.pugetsound.onebusaway.org/api/where/trips-for-route/40_100239.xml?key=TEST&includeStatus=true
 	agencyURL = 'http://api.pugetsound.onebusaway.org/api/where/trips-for-route/' + routeId + '.xml?key=' + key + '&includeStatus=true'
-	response = urlopen(agencyURL)
-	html = response.read()
-	e = ET.fromstring(html)
-	if e[1].text == '200':
-		myList = []
-		for bus in e[4][1]:
-			status = bus.find('status')
-			location = status.find('lastKnownLocation')
-			if location is not None:
-				position = status.find('position')
-				timeHeader = status.find('lastLocationUpdateTime')
-				if timeHeader is not None:
-					time = int(timeHeader.text)
-					tripId = bus.find('tripId').text
-					lat = float(position.find('lat').text)
-					lon = float(position.find('lon').text)
-					realLat = float(location.find('lat').text)
-					realLon = float(location.find('lon').text)
-					actualDistanceAlongTrip = float(status.find('distanceAlongTrip').text)
-					scheduledDistanceAlongTrip = float(status.find('scheduledDistanceAlongTrip').text)
-					totalDistanceAlongTrip = float(status.find('totalDistanceAlongTrip').text)
-					scheduleDeviation = float(status.find('scheduleDeviation').text)
-					orientation = float(status.find('orientation').text)
-					nextStop = float(status.find('nextStop').text)
-					closestStop = float(status.find('closestStop').text)
-					nextStopTimeOffset = float(status.find('nextStopTimeOffset').text)
-					closestStopTimeOffset = float(status.find('closestStopTimeOffset').text)
-					
-					myList.append( [tripId, time, lat, lon, realLat, realLon, actualDistanceAlongTrip, scheduledDistanceAlongTrip, totalDistanceAlongTrip, scheduleDeviation, orientation, nextStop, nextStopTimeOffset, closestStop, closestStopTimeOffset] )
-		return(myList)
+	successfulRead = True
+	try:
+		response = urlopen(agencyURL)
+	except:
+		successfulRead = False
+		#print("Unable to reach URL.")
+	if successfulRead:
+		html = response.read()
+		e = ET.fromstring(html)
+		if e[1].text == '200':
+			myList = []
+			for bus in e[4][1]:
+				status = bus.find('status')
+				location = status.find('lastKnownLocation')
+				if location is not None:
+					position = status.find('position')
+					timeHeader = status.find('lastLocationUpdateTime')
+					if timeHeader is not None:
+						time = int(timeHeader.text)
+						tripId = bus.find('tripId').text
+						lat = float(position.find('lat').text)
+						lon = float(position.find('lon').text)
+						realLat = float(location.find('lat').text)
+						realLon = float(location.find('lon').text)
+						actualDistanceAlongTrip = float(status.find('distanceAlongTrip').text)
+						scheduledDistanceAlongTrip = float(status.find('scheduledDistanceAlongTrip').text)
+						totalDistanceAlongTrip = float(status.find('totalDistanceAlongTrip').text)
+						scheduleDeviation = float(status.find('scheduleDeviation').text)
+						orientation = float(status.find('orientation').text)
+						nextStop = float(status.find('nextStop').text)
+						closestStop = float(status.find('closestStop').text)
+						nextStopTimeOffset = float(status.find('nextStopTimeOffset').text)
+						closestStopTimeOffset = float(status.find('closestStopTimeOffset').text)
+						
+						myList.append( [tripId, time, lat, lon, realLat, realLon, actualDistanceAlongTrip, scheduledDistanceAlongTrip, totalDistanceAlongTrip, scheduleDeviation, orientation, nextStop, nextStopTimeOffset, closestStop, closestStopTimeOffset] )
+			return([myList, ""])
+		else:
+			return([-1, "unexpected response"])
 	else:
-		return -1
+		return([-1, "unable to reach URL"])
 	
 def updateListAndSendtoDB(oldList, pointList, route, boundingBox):
 	newList = oldList #[[tripId], [firstEpochTime], [stillGettingResults], [lastUpdateTime]]
 	for index in range(len(newList[2])): #set all the variables to 0 so that we know which ones have been updated
 		newList[2][index] = 0
 	for point in pointList:
-		if (boundingBox[0] < point[4] < boundingBox[2]) and (boundingBox[1] < point[5] < boundingBox[3]): # we only want to look at the bus movements in a large rectangular area, don't want to use resources to collect all the data
+		if (boundingBox[0] < point[4] < boundingBox[2]) and (boundingBox[1] < point[5] < boundingBox[3]):
 			pointToSendToDB=[]
-			if point[0] in newList[0]: 				#See if the tripId is in our list of active trips, if it is
-				index = newList[0].index(point[0]) 		#get the index in our list
-				newList[2][index] = 1				#Set this variable to 1 to show that this trip is still being updated
-				if newList[3][index] != point[1]:		#If the time of last position update is different than the last
-					newList[3][index]=point[1]		#set the time of last position update to the new time
-					pointToSendToDB = [newList[1][index]]+point	#keep the same trip index
-					sendDataPointToDB(pointToSendToDB)		#send the point to the database
-			else: 							#if not add the new trip to the list
-				trip = [round(point[1]/1000)]+route	#the trip start information is the start time plus the route information
-				identifier = sendTripToDB(trip)			#create the trip in the trip table and return the index
-				newList[0].append(point[0]) 			#tripId
-				newList[1].append(identifier)			#index in the trip table
-				newList[2].append(1)				#trip is active
-				newList[3].append(point[1])			#last time of update
-				pointToSendToDB = [identifier]+point	#use our new trip index
-				sendDataPointToDB(pointToSendToDB)	#send point to database
-	for index in range(len(newList[2])-1, -1, -1):	#going backwards to preserve indexes
-		if oldList[2][index] == 0:					#if the point has not been updated
-			for index2 in range(len(oldList)):			#remove the point from our active list
+			if point[0] in newList[0]:
+				index = newList[0].index(point[0])
+				newList[2][index] = 1
+				if newList[3][index] != point[1]:
+					newList[3][index]=point[1]
+					pointToSendToDB = [newList[1][index]]+point
+					sendDataPointToDB(pointToSendToDB)
+			else: #if not add the new trip to the list
+				trip = [round(point[1]/1000)]+route
+				identifier = sendTripToDB(trip)
+				newList[0].append(point[0]) 
+				newList[1].append(identifier)
+				newList[2].append(1)
+				newList[3].append(point[1])
+				pointToSendToDB = [identifier]+point
+				sendDataPointToDB(pointToSendToDB)
+	for index in range(len(newList[2])-1, -1, -1):
+		if oldList[2][index] == 0:
+			for index2 in range(len(oldList)):
 				newList[index2].pop(index)
 	return newList
 
 def sendDataPointToDB(datapoint):
 	import sqlite3
 
-	db = sqlite3.connect('myDB/mydb')
+	db = sqlite3.connect('/home/john/Documents/bus/myDB/mydb')
 	cursor = db.cursor()
 	test= datapoint
 	cursor.execute(''' INSERT INTO datapoints(correspondingTripId, tripId, time, 
@@ -88,7 +97,7 @@ def sendDataPointToDB(datapoint):
 def sendTripToDB(trip):
 	import sqlite3
 
-	db = sqlite3.connect('myDB/mydb')
+	db = sqlite3.connect('/home/john/Documents/bus/myDB/mydb')
 	cursor = db.cursor()
 	test= trip
 	cursor.execute(''' INSERT INTO trips(startTime, shortName, routeId, bridgeName) VALUES(?,?,?,?)''', test)
@@ -96,19 +105,24 @@ def sendTripToDB(trip):
 	db.commit()
 	db.close()
 	return returnId
+	
+def logError(errorText):
+	with open("/home/john/Documents/bus/logs/myLog", "a+") as myfile:
+		myfile.write(errorText+"\n")
 
 def checkEndTime():
 	import datetime
 	timestamp = datetime.datetime.now().time()
-	end = datetime.time(16, 23)
+	end = datetime.time(23, 45)
 	return (timestamp <= end)
 	
 def main():
 	import time
+	import datetime
 	import csv
 
 	OVERALL_BOX = [47.548725,-122.363319,47.671399,-122.138271]
-	MY_BUS_KEY = 'TEST' 	#Replaced key with TEST so that it works but key is not revealed to the public,
+	MY_BUS_KEY = 'TEST' 	#Replaced my key with TEST so that the program works but my key is not revealed to the public,
 						#this works as a key but will often return error 503 (overload)
 
 	#[shortName, routeId, bridgeName]
@@ -151,10 +165,13 @@ def main():
 	while checkEndTime():
 		routeIndex = 0
 		for routeIndex in range(len(routeList)):
-			myNewList = getBusPositionData(routeList[routeIndex][1], MY_BUS_KEY)
+			myNewList, errorMessage = getBusPositionData(routeList[routeIndex][1], MY_BUS_KEY)
 			if myNewList == -1:
-				time.sleep(1)
-				print("Error")
+				errorString = "Error at " + str(datetime.datetime.now()) + ", " + errorMessage
+				print(errorString)
+				logError(errorString)
+				break
+				#time.sleep(3)
 			else:
 				boundingBox = OVERALL_BOX
 				myList[routeIndex] = updateListAndSendtoDB(myList[routeIndex], myNewList, routeList[routeIndex], boundingBox)
